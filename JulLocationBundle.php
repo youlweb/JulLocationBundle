119661fae258667162b893118d137ef8529a015e
Bob Dope <bob.dope@hotmail.com>
2012-11-12 18:43:24 +0100

    first commit

---
+++ JulLocationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jul\LocationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JulLocationBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Provides a location management system to use in conjonction with a map API
+
