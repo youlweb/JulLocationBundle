27aca46ceadb08d58890d6688bccdbf097014e84
youlweb <bob.dope@hotmail.com>
2013-01-28 11:49:58 +0100

    Initial commit

--- JulLocationBundle.php
+++ JulLocationBundle.php
@@ -1,9 +1,20 @@
 <?php
 
+/*
+ * JulLocationBundle Symfony package.
+ *
+ * © 2013 Julien Tord <http://github.com/youlweb/JulLocationBundle>
+ *
+ * Full license information in the LICENSE text file distributed
+ * with this source code.
+ *
+ */
+
 namespace Jul\LocationBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class JulLocationBundle extends Bundle
 {
+	
 }
