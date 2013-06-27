7b58e7b6b02a6eb9702a92ebad73c438446caa5f
codeiz <codeiz.com@gmail.com>
2013-06-27 20:51:17 +0200

    update to be compatible with new versions of symfony
    replacing getChild methods with the new property
    PropertyAccess::getValue()

--- Controller/GooglemapsController.php
+++ Controller/GooglemapsController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /*
  * JulLocationBundle Symfony package.
@@ -7,12 +7,13 @@
  *
  * Full license information in the LICENSE text file distributed
  * with this source code.
- * 
+ *
  */
 
 namespace Jul\LocationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class GooglemapsController extends Controller
 {
@@ -31,46 +32,55 @@ class GooglemapsController extends Controller
 			$maxImageHeight = 200
 	)
 	{
+        $propertyAccessor = PropertyAccess::getPropertyAccessor();
+
 		/*
 		 * Find top level entity
 		 */
 		$locationTypes = array( 'location', 'city', 'state', 'country' );
-		
 		foreach( $locationTypes as $locationType )
 		{
-			if( $locationForm->offsetExists( $locationType ) )
+			if(  array_key_exists ($locationType , $locationForm['children']))
 			{
+
 				$topLevel = $locationType;
-				$topLevelForm = $locationForm->getChild( $topLevel );
-				
+
+				$topLevelForm = $locationForm['children'][$topLevel];
+
 				break;
 			}
-			
+
 			if( $locationForm->getName() == 'Jul' . ucfirst( $locationType ) . 'Field' )
 			{
 				$topLevel = $locationType;
 				$topLevelForm = $locationForm;
-				
+
 				break;
 			}
 		}
-		
+
 		/*
 		 * Top level not found
 		 */
+
 		if( ! isset( $topLevel ) ) throw new \Exception( 'There is no location field in the form sent to the controller JulLocationBundle:Googlemaps:placesAutocomplete' );
-		
+
 		/*
 		 * Default map center and zoom
 		 */
-		if( $topLevelForm->offsetExists( 'latitude' ) && ( $latForm = $topLevelForm->getChild( 'latitude' )->get( 'value' ) ) <> 0 )
+
+
+
+		if( $topLevelForm['children']['latitude' ] && ( $latForm = $topLevelForm['children']['latitude']['vars']['value'] ) <> 0 )
 		{
+
 			/*
 			 * If the form has been sent with a location
 			 */
 			$latitude = $latForm;
-			$longitude = $topLevelForm->getChild( 'longitude' )->get( 'value' );
-			
+
+			$longitude = $topLevelForm['children']['longitude']['vars']['value'];
+
 			$zoomDefault = $zoomResolved;
 		}
 		else
@@ -79,22 +89,24 @@ class GooglemapsController extends Controller
 			if( ! $longitude ) $longitude = -98.7372;
 			if( ! $zoomDefault ) $zoomDefault = 3;
 		}
-		
+
 		/*
 		 * Default map options array
 		 */
 		$mapOptions = array_merge( array(
 			'zoom' => $zoomDefault
 			), $mapOptions );
-		
+
 		/*
 		 * Default autocomplete input field
 		 */
 		if( ! isset( $acFields[ 0 ][ 'acInput' ] ) )
 		{
-			$acFields[ 0 ][ 'acInput' ] = ( $topLevelForm->offsetExists( 'long_name' ) ) ? $topLevelForm->getChild( 'long_name' )->get( 'id' ) : $topLevelForm->getChild( 'name' )->get( 'id' );
+
+			$acFields[ 0 ][ 'acInput' ] = ( array_key_exists ('long_name' , $topLevelForm['children']) ) ? $topLevelForm['children']['long_name']['vars']['id'] : $topLevelForm['children']['name']['vars']['id'];
+
 		}
-		
+
 		/*
 		 * Default autocomplete Types
 		 */
@@ -107,46 +119,46 @@ class GooglemapsController extends Controller
 				default: $acFields[ 0 ][ 'acOptions' ][ 'types' ] = array( '(regions)' );
 			}
 		}
-		
+
 		/*
 		 * Address autocomplete fallback
 		 */
 		if( $addressFallback && $topLevel == 'location' && ! isset( $acFields[ 1 ][ 'acInput' ] ) && $topLevelForm->offsetExists( 'long_address' ) )
 		{
-			$acFields[ 1 ][ 'acInput' ] = ( $topLevelForm->offsetExists( 'long_name' ) ) ? $topLevelForm->getChild( 'long_address' )->get( 'id' ) : $topLevelForm->getChild( 'address' )->get( 'id' );
+			$acFields[ 1 ][ 'acInput' ] = ( $topLevelForm->offsetExists( 'long_name' ) ) ? $propertyAccessor->getValue($topLevelForm , 'children[long_address.id]') : $propertyAccessor->getValue($topLevelForm , 'children[address.id]');
 			$acFields[ 1 ][ 'acOptions' ][ 'types' ] = array( 'geocode' );
 		}
-		
+
 		/*
 		 * Build javascript field IDs array using JulLocationBundle config
 		 */
-		
+
 		$jsFieldIds = array();
 		$tmpLevel = $locationForm;
-		
+
 		foreach( $this->container->parameters[ 'jul_location.options' ] as $level => $options )
 		{
 			$fields = $options['fields'];
-			
 			$tmpArray = array();
-			
-			if( $tmpLevel->offsetExists( $level ) )
-			{	
-				$tmpLevel = $tmpLevel->getChild( $level );
-				
+			if( array_key_exists ($level  , $tmpLevel['children']) )
+			{
+
+				$tmpLevel = $tmpLevel['children'][$level];
+
 				foreach( $fields as $field => $fieldArray )
 				{
 					/*
 					 * Check if field is active in config && exists in the form
 					 */
-					if( $fieldArray[ 'enabled' ] && $tmpLevel->offsetExists( $field ) ) $tmpArray[ $field ] = $tmpLevel->getChild( $field )->get( 'id' );
+
+					if( $fieldArray[ 'enabled' ] && array_key_exists ($field  , $tmpLevel['children'])  ) $tmpArray[ $field ] = $tmpLevel['children'][ $field ]['vars']['id'] ;
+
 				}
 			}
-			
+
 			$jsFieldIds[ $level ] = $tmpArray;
 		}
-		
-		return $this->render( 'JulLocationBundle:Googlemaps:placesAutocomplete.html.twig', array(				
+return $this->render( 'JulLocationBundle:Googlemaps:placesAutocomplete.html.twig', array(
 				'mapDiv' => $mapDiv,
 				'mapOptions' => json_encode( $mapOptions ),
 				'acFields' => json_encode( $acFields ),
